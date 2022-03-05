42df4d4f1236b73acb6ec268c1fdf30ec281427d
Suman Ghosh <sumanghosh.aot@gmail.com>
2022-03-05 21:13:37 +0530

    Update Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ export const Footer = () => {
   return (
     <div className="footer">
         <div>Movie App</div>
-        <div>©2022, Movie, Inc. or its affiliates</div>
+        <div>©2022, Movie, Inc</div>
     </div>
   )
 }
